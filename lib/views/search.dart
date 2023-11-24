f9b5df74a73ff3e8a1bf46ddb648132b2829aefa
Anshuman Sharma <anshuman9998@gmail.com>
2023-11-25 00:46:04 +0530

    search button

--- lib/views/search.dart
+++ lib/views/search.dart
@@ -41,6 +41,38 @@ class Search extends StatelessWidget {
                 ),
               ),
             ),
+            const SizedBox(
+              height: 50,
+            ),
+            Container(
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(3),
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1,
+                  ),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 0,
+                      offset: Offset(4, 4),
+                    ),
+                  ],
+                ),
+                child: SizedBox(
+                  height: 50,
+                  child: MaterialButton(
+                    onPressed: () {},
+                    color: const Color(0xff014bf4),
+                    child: const Center(
+                      child: Text(
+                        "Search",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ))
           ],
         ),
       ),
